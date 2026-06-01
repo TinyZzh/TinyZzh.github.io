1ef592e4d5222d3b70ac1347b6771cf0e09875bd
TinyZ <tinyzzh815@gmail.com>
2026-06-02 00:29:39 +0800

    fix: 修复 siteConfig.ts biome 格式检查报错

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -16,18 +16,11 @@ export const siteConfig: SiteConfig = {
 	site_url: "https://tinyzzh.github.io",
 
 	// 站点描述
-	description: "专注于网络游戏前后端技术(JAVA, PHP, Unity3D)。积累技术,记录分享。",
+	description:
+		"专注于网络游戏前后端技术(JAVA, PHP, Unity3D)。积累技术,记录分享。",
 
 	// 站点关键词
-	keywords: [
-		"TinyZ",
-		"博客",
-		"技术博客",
-		"Java",
-		"PHP",
-		"Unity3D",
-		"游戏开发",
-	],
+	keywords: ["TinyZ", "博客", "技术博客", "Java", "PHP", "Unity3D", "游戏开发"],
 
 	// 主题色
 	themeColor: {
